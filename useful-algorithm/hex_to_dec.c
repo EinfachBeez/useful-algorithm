561f3b77c357a0c448075d135a3513a81e8798f9
EinfachBeez <56166282+EinfachBeez@users.noreply.github.com>
2021-09-26 15:43:59 +0200

    added hex to dec, updated ignore

--- useful-algorithm/hex_to_dec.c
+++ useful-algorithm/hex_to_dec.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
+#define _CRT_SECURE_NO_WARNINGS // avoid windows compiler warnings
 
 int main() {
+    // 16 Digits = hexadecimal from 0 to 9 and from A to F
+    char hex[16];
+    int dec = 0, i, k, n = 0;
 
-	// ToDo
+    printf("Heximal Input: ");
+    scanf("%s", hex);
 
-	return 0;
+    for (i = strlen(hex) - 1; i >= 0; --i) {
+
+        if (hex[i] >= '0' && hex[i] <= '9') {
+            k = hex[i] - '0';
+        }
+        else {
+            k = hex[i] - 'A' + 10;
+        }
+
+        dec += k * pow(16, n);
+        ++n;
+    }
+
+    printf("Decimal Value: %d", dec);
+
+    return 0;
 }
